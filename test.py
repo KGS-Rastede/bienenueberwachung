6530ffd40eb334418631a3f20fe789c5a91a2f70
KGS-Rastede <ni@kgs-rastede.de>
2016-03-13 15:51:46 +0100

    Update test.py
    Programmlogik vielleicht falsch. Siehe Kommentar

--- test.py
+++ test.py
@@ -5,6 +5,16 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+
+# Ich denke, dass hier die Programmlogik falsch ist:
+#
+#>>> while True:
+#...     try:
+#...         x = int(input("Please enter a number: "))
+#...         break
+#...     except ValueError:
+#...         print("Oops!  That was no valid number.  Try again...")
+#...
 try:
 	while True:
 		if (GPIO.input(11) == 1):
