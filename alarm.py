e244eeb1fef68bc7a1db07e4391355801e1dd696
KGS-Rastede <ni@kgs-rastede.de>
2016-03-13 21:37:18 +0100

    Added files via upload
    GPIO-Pins sind nun im Programm benutzt,
    warten.py startet bei Unterbrechung des Stromkreises alarm.py,
    Der Countdown von alarm.py lässt sich durch Wiederherstellung des Stromkreises unterbrechen und startet warten.py

--- alarm.py
+++ alarm.py
@@ -1,20 +1,28 @@
-#Letzte Änderungen: Login-Daten in CSV-Datei und Timerfunktion
+#Letzte Aenderungen: Login-Daten in CSV-Datei und Timerfunktion
 #Todo: GPIO-Pins,	Stromkreis unterbrochen = Benachrichtigung
 #			Stromkreis wiederhergestellt = Timer unterbrochen
 
-#Importierung der Simple Mail Transfer Protocol Libary (SMTP)
+#Importierung der Simple Mail Transfer Protocol (SMTP), CSV Libaries und Time/Sleep Funktion
+#!/usr/bin/python
 from time import sleep
 import smtplib
 import csv
-#--------------------------Deklarierung einiger nützlicher Variablen-------------------------
-	#Einbindung der Login-CSV-Datei, Pfad kann geändert werden
-with open('/mnt/Steffweiterung/Schule/Seminarfach/Facharbeit/bienenueberwachung/login.csv') as csvfile:
+import RPi.GPIO as GPIO
+import subprocess
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.IN)
+#--------------------------Deklarierung einiger nuetzlicherVariablen-------------------------
+	#Einbindung der Login-CSV-Datei, Pfad kann geaendert werden
+with open('/home/bienenpi/login.csv') as csvfile:
 	readCSV = csv.reader(csvfile, delimiter=',')
 	for row in readCSV:
 		User = row[0]
 		Pass = row[1]
 
-	#Variable zur Abkürzung von 'smtplib.SMTP('smtp.gmail.com',587)' zu 'server'
+	#Variable zur Abkuerzung von 'smtplib.SMTP('smtp.gmail.com',587)' zu 'server'
 server = smtplib.SMTP('smtp.gmail.com',587)
 
 	#Die Variable 'empfaenger' ist das Ziel der Benachrichtigungsmail
@@ -25,19 +33,24 @@ empfaenger = 'steffenistcool@gmail.com'
 
 #----------------------Formatierung und Senden der Benachrichtigungsmail---------------------
 	#Emailformatierung: Betreff, header und body der Mail (bisher nur Platzhalter)
-subject = 'Bienenstockalarm'
+subject = 'Bienenprobleme'
 header = 'To: ' + empfaenger + '\n' + 'From: ' + User + '\n' +'Subject: ' + subject
-body = 'Anscheinend ist etwas los, beim Bienenstock.'
-
+body = 'Anscheinend sind die Bienenkaesten umgefallen.'
+	#startet Timer fuer das Senden der Mail
 for Timer in range(0,60):
+	#Timer zaehlt jede Sekunde runter
 	print(60 - Timer)
 	sleep(1)
+	#wenn Stromkreis geschlossen, starte Warten-Skript
+	if (GPIO.input(18) == 1):
+		print('Verbindung wiederhergestellt')
+		status = subprocess.call("sudo python" + " /home/bienenpi/warten.py", shell=True)
 
 	#Handshake mit dem Googlemail-Server
 server.ehlo()
-	#Herstellung einer Verschlüsselung zur sicheren Datenübertragung
+	#Herstellung einer Verschluesselung zur sicheren Datenuebertragung
 server.starttls()
-	
+
 	#Authentifizierung des Mailaccounts
 server.login(User, Pass)
 	#Senden der Mail
@@ -45,5 +58,9 @@ server.sendmail(User, empfaenger, header + '\n\n'+ body)
 
 #beendet die Verbindung zum Googlemail-Server
 server.quit()
+
+#startet das Warten-Skript, wenn Mail gesendet
+status = subprocess.call("sudo python" + " /home/bienenpi/warten.py", shell=True)
+
 
 #-------------------------------------------------------------------------------------------
---
+++ warten.py
@@ -0,0 +1,31 @@
+#----------------Importierung nuetzlicher Variablen und Libaries--------------
+#!/usr/bin/python
+import os
+import RPi.GPIO as GPIO
+import time
+import subprocess
+#Waehlen des GPIO-Pin Layouts (GPIO.BOARD oder GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setwarnings(False)
+#Setup des/der zu benutzenden Pins
+GPIO.setup(18,GPIO.IN)
+
+#Derzeitiger Status des Pins (nuetzlich, um Warten bei Fehlern vorzubeugen)
+print GPIO.input(18)
+#While-Schleife zum Pruefen, ob Stromkreis geschlossen ist
+while True:
+	#wenn Stromkreis offen
+	if (GPIO.input(18) == 0):
+		#gib 'Unterbrochen' aus
+		print('Unterbrochen')
+		#gib Datum und Zeit an
+		os.system('date')
+		#gib Status von Pin 18 an, um zu verdeutlichen, dass Stromkreis offen ist
+		print GPIO.input(18)
+		#warte 5 Sekunden
+		time.sleep(5)
+		#starte Skript zum Senden von Mail
+		status = subprocess.call("python" + " /home/bienenpi/alarm.py", shell=True)
+
+#-----------------------------------------------------------------------------
